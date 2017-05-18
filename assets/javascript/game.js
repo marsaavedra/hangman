406b7dffc45c371d4b113dde0428a79adbd456ee
maria saavedra <mariasaavedra@marias-MacBook-Air.local>
2017-05-17 21:42:52 -0400

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // javascipt file
 
 //the very first thing I want to create is my word bank and hints
-function wordBank () {
+
 var hangmanGame = {
     words: [
         {
@@ -29,12 +29,74 @@ var hangmanGame = {
 
 
 //randomize the word bank
-var randomWord = hangmanGame.words[Math.floor(Math.floor()* hangmanGame.words.length)].word;
+var randomNum = Math.floor(Math.random() * hangmanGame.words.length); //this variable was seperated for simplicity
+var randomWord = hangmanGame.words[randomNum].word;
+var randomWordHint = hangmanGame.words[randomNum].hint;
 console.log("randomWord: ", randomWord);
+console.log("randomWordHint: ", randomWordHint);
 
-document.getElementById("curWord").innerHTML = randomWord; 
 
+//empty array to store the current word (word the user is trying to guess to win)
+var curWordArray = [];
+//empty array for letters already guessed
+var lettersGuessedArray = [];
+//s for string 
+var s; 
+
+//the variables that contain numbers
+var wins = 0;
+var numberOfGuessesLeft = 15;
+
+//putting the correct actions in the correct places in the HTML file
+document.getElementById("wins").innerHTML = wins;
+
+document.getElementById("hint").innerHTML = randomWordHint;
+
+document.getElementById("numberOfGuessesLeft").innerHTML = numberOfGuessesLeft;
+
+document.getElementById("curWord").innerHTML = curWordArray;
+//this section is to get the dashes for the user to guess
+    for (var i = 0; i < randomWord.length; i++) {
+        // console.log("randomWord[i]", randomWord[i]);
+        curWordArray[i] = "_";
+        
+    }
+    s= curWordArray.join(" ");
+    document.getElementById("curWord").innerHTML = s;
+
+//the next step to is to include user input
+document.onkeyup = function (event) {
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+   
+    for(var i=0; i < randomWord.length; i++) {
+        if(randomWord[i] === letter) {
+            curWordArray[i] = letter;
+            console.log("letterpressed: ", curWordArray[i]);
+                s= curWordArray.join(" ");
+                document.getElementById("curWord").innerHTML = s;
+       
+        }if (randomWord[i] !== letter) {
+            lettersGuessedArray[i] === letter;
+            console.log("letterpressed: ", lettersGuessedArray[i]);
+            s= lettersGuessedArray.join(" ");
+                document.getElementById("lettersGuessed").innerHTML = s;
+        }
+        } 
 };
+        //money shot right here..if the random word contains the letter the user typed in..
+        // if (randomWord[i] === letter) {
+        //     //BOOM! Assign it to the dang letter
+        //     curWordArray[i] = letter;
+        //     console.log("right letter:", letter);
+        // }
+        // else {
+        //     console.log("wrongLetter:", letter);
+        //     guessedArray[i] = letter;
+        //     // document.getElementById("lettersGuessed").innerHTML = guessedArray;
+        //     guessedArray.push[letter];
+        // }
+//     }
+// };
 
-console.log ("randomWord");
+
 
